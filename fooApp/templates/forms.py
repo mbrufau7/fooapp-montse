3d67cb192b98e15eec0ada87e1e55791c1b18647
Montse Brufau <montserratbrufau@gmail.com>
2017-11-24 18:02:28 +0100

    web page commit

---
+++ fooApp/templates/forms.py
@@ -0,0 +1,9 @@
+from wtforms import PasswordField
+
+class LoginForm(Form):
+    """Render HTML input for user login form.
+
+    Authentication (i.e. password verification) happens in the view function.
+    """
+    username = TextField('Username', [required()])
+    password = PasswordField('Password', [required()])
