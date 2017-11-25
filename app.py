ff65771073a8cc664aa4ab63696d8b66c168e5f4
Montse Brufau <montserratbrufau@gmail.com>
2017-11-25 17:42:26 +0100

    .forms to forms

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from flask_login import LoginManager, current_user
 from flask_login import login_user, logout_user
 from flask_login import login_required
 
-from .forms import LoginForm
-from .model import User
+from forms import LoginForm
+from model import User
 
 import json
 
